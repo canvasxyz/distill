9957bfb403f413b9e33ae617b8b365df47a452e3
Raymond Z <raykyri@gmail.com>
2025-10-20 14:01:28 -0400

    update vite.config for vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/archive-explorer/",
-  plugins: [react()],
+export default defineConfig(() => {
+  const isVercel = Boolean(process.env.VERCEL);
+
+  return {
+    base: isVercel ? "/" : "/archive-explorer/",
+    plugins: [react()],
+  };
 });
