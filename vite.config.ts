9dd56213b7a290500cfd08eba8dfb313eea888da
Raymond Z <raykyri@gmail.com>
2025-10-21 01:52:09 -0400

    deployment config for cf pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig(() => {
   const isVercel = Boolean(process.env.VERCEL);
+  const isCloudflarePages = Boolean(process.env.CF_PAGES);
 
   return {
-    base: isVercel ? "/" : "/archive-explorer/",
+    base: isVercel || isCloudflarePages ? "/" : "/archive-explorer/",
     plugins: [react()],
   };
 });
