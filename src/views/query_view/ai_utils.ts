0d02a1b7c50b0147f3db3ae07c970d39ae77285f
Bob Webb <bob.wbb@gmail.com>
2025-11-10 11:57:23 +0100

    pass retweet count to llm

--- src/views/query_view/ai_utils.ts
+++ src/views/query_view/ai_utils.ts
@@ -78,7 +78,7 @@ export function makePromptMessages(
       content: tweetsSample
         .map(
           (tweet) =>
-            `<Post id="${tweet.id_str}" date="${tweet.created_at}" num_likes="${tweet.favorite_count}">${tweet.full_text}</Post>`,
+            `<Post id="${tweet.id_str}" date="${tweet.created_at}" num_likes="${tweet.favorite_count}" num_retweets="${tweet.retweet_count}">${tweet.full_text}</Post>`,
         )
         .join("\n"),
     },
